45b330d0f83f8e0ec4b7f850d1fd2199a513cb69
anyuruf <anyuruf@gmail.com>
2021-12-17 13:22:34 +0300

    Setup: toggle  show user menu function

--- app/components/base/nav.js
+++ app/components/base/nav.js
@@ -1,12 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class NavComponent extends Component {
   @service session;
+  @tracked showMenu = false;
 
   @action
   invalidateSession() {
     this.session.invalidate();
+  }
+
+  @action
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
 }
