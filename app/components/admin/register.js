2e35e19b3c0126c4e26a349f7f3813554d139d83
anyuruf <anyuruf@gmail.com>
2021-12-28 16:19:29 +0300

    Feat: add-user route done

--- app/components/admin/register.js
+++ app/components/admin/register.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { useMutation } from 'glimmer-apollo';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { SIGN_UP } from '../gql/mutations/sign-up';
+import { SIGN_UP } from '../../gql/mutations/sign-up';
 
 export default class AdminRegisterComponent extends Component {
   @tracked firstName;
@@ -22,4 +22,17 @@ export default class AdminRegisterComponent extends Component {
       },
     },
   ]);
+
+  @action
+  async createUser(e) {
+    e.preventDefault();
+
+    await this.SignUp.mutate({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      role: this.role,
+      password: this.password,
+    });
+  }
 }
